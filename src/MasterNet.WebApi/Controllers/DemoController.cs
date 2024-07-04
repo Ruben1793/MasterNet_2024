76f43e5ac6412b191d246da704de01cf2c9a5106
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2024-07-03 19:29:49 -0600

    Ambientes de desarrollo

--- src/MasterNet.WebApi/Controllers/DemoController.cs
+++ src/MasterNet.WebApi/Controllers/DemoController.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MasterNet.WebApi.Controllers;
@@ -5,11 +6,27 @@ namespace MasterNet.WebApi.Controllers;
 [ApiController]
 [Route("Demo")]
 public class DemoController : ControllerBase {
-        
 
-        [HttpGet("getstring")]
+    private readonly IConfiguration _configuration;
+
+    private readonly IHostEnvironment _enviroment;
+
+    public DemoController(IConfiguration configuration, IHostEnvironment enviroment)
+    {
+        _configuration = configuration;
+        _enviroment = enviroment;
+    }
+
+    [HttpGet("getstring")]
         public string GetNombre() {
             return "Ruben";
+        }
+
+        [HttpGet("ambiente")]
+        public IActionResult GetAmbiente() {
+            var mensaje = _configuration.GetValue<string>("MiVariable");
+            var ambiente = _enviroment.EnvironmentName;
+            return Ok(new { Ambiente = ambiente, Mensaje = mensaje});
         }
         
 }
