2e6153c7634b8fafaff3ae83d947431c6fa8f09b
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2024-10-03 19:21:18 -0600

    Pagination Calificaciones

---
+++ src/MasterNet.Application/Calificaciones/GetCalificaciones/GetCalificaionesRequest.cs
@@ -0,0 +1,10 @@
+using MasterNet.Application.Core;
+
+namespace MasterNet.Application.Calificaciones.GetCalificaciones;
+
+public class GetCalificacionesRequest: PagingParams
+{
+    public string? Alumno {get; set;}
+
+    public Guid? CursoId {get; set;}
+}
